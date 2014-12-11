162b53a791a3ecdcd498215445d087591071b353
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2014-12-11 10:10:40 +0800

    改改改

--- makesql.py
+++ makesql.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import MySQLdb
+from datetime import date
+date = date.today()
+print date
 def makesql():
     con =MySQLdb.connect(host="localhost",user="root",passwd="root",\
             db="order_food",charset="utf8")
@@ -12,7 +15,7 @@ def insert_seat(position_message):
     make,con = makesql()
     print position_message
     try:
-        make.execute("insert into reservation (phone,user_name,come_date,come_people,position,other) values (%s,%s,%s,%s,%s,%s)",position_message)
+        make.execute("insert into reservation (phone,user_name,come_date,come_time,come_people,position,other) values (%s,%s,%s,%s,%s,%s,%s)",position_message)
         con.commit()
         return "OK"
     except Exception,e:
@@ -35,8 +38,9 @@ def insert_user(user_message):
 def select_seat():
     make,con = makesql()
     try:
-        make.execute("select phone,user_name,come_date,come_people,position,\
-                other from reservation where review='pending'")
+        make.execute("select phone,user_name,come_date,come_time,come_people,position,\
+                other from reservation where review='pending' \
+                and come_date=%s",date)
         fetchall = make.fetchall()
         return fetchall
     except Exception,e:
@@ -45,11 +49,25 @@ def select_seat():
     con.close()
 
 
-def seat_allow(phone):
+def select_allowseat():
+    make,con = makesql()
+    try:
+        make.execute("select phone,user_name,come_date,come_time,come_people,position,\
+                other from reservation where review='allow' and \
+                come_date=%s",date)
+        fetchall = make.fetchall()
+        return fetchall
+    except Exception,e:
+        return e
+    make.close()
+    con.close()
+
+
+def seat_allow(phone,date):
     make,con = makesql()
     try:
         result = make.execute("update reservation set review='allow' where \
-                phone=%s",phone)
+                phone=%s and come_date=%s",(phone,date))
         con.commit()
         return result
     except Exception,e:
