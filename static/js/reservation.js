aa6f3a5abbe8491c5aa7d9bd9328d513bb535ca7
mingyili <mingyili@dnspod.com>
2015-01-22 11:14:47 +0800

    add form verify

--- static/js/reservation.js
+++ static/js/reservation.js
@@ -27,6 +27,42 @@ $("#come_data_3").click(function(){
     $("#come_data_1").css('background','#fff');
     $("#come_data_2").css('background','#fff');
 });
+// change postData['come_time']
+var hour = null;
+var minute = null;
+$("#come_time_hour").change(function(){
+    hour = $(this).val();
+    if (hour && minute) {
+        postData['come_time'] = hour + ':'+minute+':00';
+        console.log(postData['come_time']);
+    }
+});
+$("#come_time_minute").change(function(){
+    minute = $(this).val();
+    if (hour && minute) {
+        postData['come_time'] = hour + ':'+minute+':00';
+        console.log(postData['come_time']);
+    }
+});
+// change postData['user_name']
+$("#userName").change(function(){
+    postData['user_name'] = $(this).val()
+});
+// change postData['phone']
+$("#phoneNumber").change(function(){
+    postData['phone'] = $(this).val()
+});
+// change postData['come_people']
+$("#comePeople").change(function(){
+    postData['come_people'] = $(this).val();
+});
+// change postData['room_type']
+$("$roomType").change(function(){
+    postData['room_type'] = $(this).val();
+});
+$("$otherContent").change(function(){
+    postData['other'] = $(this).val();
+});
 // submit form
 $("#submit_btn").click(function(){
     var r_varify = varifty(postData);
@@ -38,7 +74,6 @@ $("#submit_btn").click(function(){
             alert(r_varify['string']);
         }
     } else if (r_varify['status'] == 'success') {
-        postData['other'] = $('#otherContent').val();
         $.post(
             '/reservation',
             postData,
@@ -55,47 +90,45 @@ var varifty = function(post_data){
         r_data['string'] = 'open_id is null';
         r_data['dome_id'] = '';
         return r_data;
-    }
-    if (!post_data['user_status']) {
+    } else if (!post_data['user_status']) {
         r_data['status'] = 'error';
         r_data['string'] = 'user_status is null';
         r_data['dome_id'] = '';
         return r_data;
-    }
-    if (!post_data['user_name']) {
+    } else if (!post_data['user_name']) {
         r_data['status'] = 'error';
         r_data['string'] = 'user_name is null';
         r_data['dome_id'] = '#userName';
         return r_data;
-    }
-    if (!post_data['phone']) {
+    } else if (!post_data['phone']) {
         r_data['status'] = 'error';
         r_data['string'] = 'phone is null';
         r_data['dome_id'] = 'phoneNumber';
         return r_data;
-    }
-    if (!post_data['come_date']) {
+    } else if (!post_data['come_date']) {
         r_data['status'] = 'error';
         r_data['string'] = 'come_date is null';
         r_data['dome_id'] = '';
         return r_data;
-    }
-    if (!post_data['come_time']) {
+    } else if (!post_data['come_time']) {
         r_data['status'] = 'error';
         r_data['string'] = 'come_time is null';
         r_data['dome_id'] = '';
         return r_data;
-    }
-    if (!post_data['come_people']) {
+    } else if (!post_data['come_people']) {
         r_data['status'] = 'error';
         r_data['string'] = 'come_people is null';
         r_data['dome_id'] = '#comePeople';
         return r_data;
-    }
-    if (!post_data['room_type']) {
+    } else if (!post_data['room_type']) {
         r_data['status'] = 'error';
         r_data['string'] = 'room_type is null';
         r_data['dome_id'] = 'roomType';
         return r_data;
+    else{
+        if (!post_data['other']) {
+            post_data['other'] = null;
+        }
+        r_data['status'] = 'success';
     }
 }
